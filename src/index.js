7eb079b3cfe4231ee0eb01c72e8f024990354e57
anthonylan <anthonylannn@gmail.com>
2019-06-21 16:18:30 +0200

    K

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import {TweenMax, Power2, TimelineLite, Expo, Back, TweenLite, Power4} from "gsap/TweenMax";
+import {TweenMax, Power2, TimelineLite, Expo, Back, TweenLite, Power4} from "gsap/TweenMax"
+import ScrollMagic from 'scrollmagic'
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
+
 
 document.querySelector('button').addEventListener('click', (e) => {
     e.preventDefault()
