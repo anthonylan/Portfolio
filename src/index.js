31eaf15b416e8ee3cf929d0a570f6dac1db72e91
anthonylan <anthonylannn@gmail.com>
2019-06-21 13:27:00 +0200

    G

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert('Under Construction...')
+import {TweenMax, Power2, TimelineLite, Expo, Back, TweenLite, Power4} from "gsap/TweenMax";
+
+document.querySelector('button').addEventListener('click', (e) => {
+    e.preventDefault()
+
+    TweenMax.to('h1', .5, {
+        x: 300
+    })
+})
